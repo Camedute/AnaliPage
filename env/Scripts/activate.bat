0090d6b960fb5fe1bccbefc7fb0020713cd4c34f
Jordan Flores Pereira <Jord.floresp@duocuc.cl>
2025-01-12 17:18:15 -0300

    primer

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\camed\Desktop\portafolio\analiPage\env
+set VIRTUAL_ENV=C:\Users\flore\OneDrive\Escritorio\ProyectoMario\AnaliPage\env
 
 if not defined PROMPT set PROMPT=$P$G
 
