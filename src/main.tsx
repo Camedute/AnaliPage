3c25f7d1b6208620985dfc8fd408385be4220f30
Mario Norambuena Espinosa <ma.norambuenae@duocuc.cl>
2025-01-14 17:49:25 -0300

    añadimiento de componente a main

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import Login from './Login.js';
+import Formulario from './Components/Registro.js';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Login />
+    <Formulario />
   </StrictMode>,
 )
