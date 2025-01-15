e85d9bbd9942ba798c6164dd537d985f3a037a61
Jordan Flores Pereira <Jord.floresp@duocuc.cl>
2025-01-15 10:38:13 -0300

    mejoras rutas

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from "react-router-dom";
-import './index.css'
-import Login from './Login.js';
-import Formulario from './Components/Registro.js';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+import Login from './Login';
+import Registro from './Components/Registro';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <Formulario />
+      <Routes>
+        {/* Ruta por defecto a Login */}
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/Registro" element={<Registro />} />
+      </Routes>
     </BrowserRouter>
   </StrictMode>,
-)
+);
